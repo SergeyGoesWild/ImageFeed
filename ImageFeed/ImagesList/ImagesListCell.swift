b3d304d30f258807618e0d6fc0739ee601dd6be7
Sergey Telnov <sergej.telnov@gmail.com>
2024-09-01 23:47:32 +0200

    Finished the 8th sprint

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Sergey Telnov on 30/08/2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bgImage: UIImageView!
+}
