482dbbc35817231ec0d8eb72118343b0b27fc758
Sergey Telnov <sergej.telnov@gmail.com>
2024-09-05 13:13:01 +0200

    Сделал экран пользователя, проверил по макету

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Sergey Telnov on 05/09/2024.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
