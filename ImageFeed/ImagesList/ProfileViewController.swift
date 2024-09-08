402607f70241c842a3f132759182f800cdc81824
Sergey Telnov <sergej.telnov@gmail.com>
2024-09-08 21:36:41 +0200

    Поместил вызовы элементов в функции, добавил небольшие изменения в код (NSLayoutConstraint для всех констрейнтов сразу)

--- ImageFeed/ImagesList/ProfileViewController.swift
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -13,26 +13,11 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let UserPicture = UIImage(named: "ProfilePhoto")
-        let imageView = UIImageView(image: UserPicture)
-        
-        let nameLabel = UILabel()
-        nameLabel.textColor = .white
-        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .semibold)
-        nameLabel.text = "Екатерина Новикова"
-        
-        let tagLabel = UILabel()
-        tagLabel.textColor = UIColor(red: 0.68, green: 0.69, blue: 0.71, alpha: 1.00)
-        tagLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        tagLabel.text = "@ekaterina_nov"
-        
-        let textLabel = UILabel()
-        textLabel.textColor = .white
-        textLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        textLabel.text = "Hello, world!"
-        
-        let exitIcon = UIImage(named: "ExitButton")
-        let exitButton = UIButton.systemButton(with: exitIcon!, target: self, action: #selector(Self.didTapExitButton))
+        let imageView = spawnProfilePicture(profilePictureName: "ProfilePhoto")
+        let nameLabel = spawnNameLabel(userName: "Екатерина Новикова")
+        let tagLabel = spawnTagLabel(profileTag: "@ekaterina_nov")
+        let textLabel = spawnTextLabel(profileText: "Hello, world!")
+        let exitButton = spawnExitButton(iconName: "ExitButton")
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -46,21 +31,62 @@ class ProfileViewController: UIViewController {
         view.addSubview(textLabel)
         view.addSubview(exitButton)
         
-        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        tagLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        textLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            tagLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            textLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            tagLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            textLabel.topAnchor.constraint(equalTo: tagLabel.bottomAnchor, constant: 8),
+            exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ])
         
-        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52).isActive = true
-        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
-        tagLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
-        textLabel.topAnchor.constraint(equalTo: tagLabel.bottomAnchor, constant: 8).isActive = true
-        exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
     }
     
     @objc
-    func didTapExitButton() {
+    private func didTapExitButton() {
         print("pressed EXIT button")
     }
+    
+    private func spawnProfilePicture(profilePictureName: String) -> UIImageView {
+        let UserPicture = UIImage(named: profilePictureName)
+        let imageView = UIImageView(image: UserPicture)
+        return imageView
+    }
+    
+    private func spawnNameLabel(userName: String) -> UILabel {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .semibold)
+        nameLabel.text = userName
+        return nameLabel
+    }
+    
+    private func spawnTagLabel(profileTag: String) -> UILabel {
+        let tagLabel = UILabel()
+        tagLabel.textColor = UIColor(red: 0.68, green: 0.69, blue: 0.71, alpha: 1.00)
+        tagLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        tagLabel.text = profileTag
+        return tagLabel
+    }
+    
+    private func spawnTextLabel(profileText: String) -> UILabel {
+        let textLabel = UILabel()
+        textLabel.textColor = .white
+        textLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        textLabel.text = profileText
+        return textLabel
+    }
+    
+    private func spawnExitButton(iconName: String) -> UIButton {
+        let exitIcon = UIImage(named: iconName)
+        let backupIcon = UIImage(systemName: "xmark.square")
+        let exitButton = UIButton.systemButton(with: exitIcon ?? backupIcon!, target: self, action: #selector(Self.didTapExitButton))
+        return exitButton
+    }
+    
 }
