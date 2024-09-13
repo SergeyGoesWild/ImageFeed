aa369fb958f41d7602a0436841c24b3b7127bcd5
Sergey Telnov <sergej.telnov@gmail.com>
2024-09-13 14:34:32 +0200

    Закончил работу над ошибками, собираюсь сдавать 9ый спринт

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -24,67 +24,28 @@ final class SingleImageViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
     
     override func viewDidLoad() {
-//        Просто пытаюсь отцентрировать изображение, пока без масштабирования, в оригинальном размере, чтобы было проще.
         super.viewDidLoad()
+        scrollView.delegate = self
         guard let image = image else { return }
         imageView.image = image
-        imageView.layer.borderColor = UIColor.red.cgColor
-        imageView.layer.borderWidth = 2
-//        imageView.frame.size = image.size
-//        scrollView.contentSize = image.size
-//        let newX = abs(scrollView.frame.width / 2 - imageView.frame.size.width / 2)
-//        let newY = abs(scrollView.frame.height / 2 - imageView.frame.size.height / 2)
-//        scrollView.contentOffset = CGPoint(x: newX, y: newY)
-//        rescaleAndCenterImageInScrollView(image: image)
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
-        // Now the scrollView frame is set, and we can get its size
         guard let image = image else { return }
-        let ratio = image.size.height / image.size.width
-        
-        imageView.frame.size.width = scrollView.frame.width
-        imageView.frame.size.height = imageView.frame.size.width * ratio
-        
-        scrollView.contentSize = scrollView.frame.size
-        
-        let newY = scrollView.frame.height / 2 - imageView.frame.size.height / 2
-        scrollView.contentOffset = CGPoint(x: 0, y: -newY)
-        
-        print("new Y : ", newY)
-        print(ratio)
-        print(imageView.frame.size.width, " x ", imageView.frame.size.height)
-        print(scrollView.frame.size.width, " x ", scrollView.frame.size.height)
-//        imageView.frame.size = image.size
-//        scrollView.contentSize = image.size
-//        let newX = abs(scrollView.frame.width / 2 - imageView.frame.size.width / 2)
-//        let newY = abs(scrollView.frame.height / 2 - imageView.frame.size.height / 2)
-//        scrollView.contentOffset = CGPoint(x: newX, y: newY)
-//        print(imageView.frame.size.width, " x ", imageView.frame.size.height)
-//        print(scrollView.frame.size.width, " x ", scrollView.frame.size.height)
-//        print(newX, newY)
-
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
+    //        Комментарий для проверяющего. Авторское решение, предложенное в уроке для метода rescaleAndCenterImageInScrollView не сработало у меня, даже если копировал-вставлял весь класс. Почему, пока не разобрался. Но написал свой метод, надеюсь, что вас это устроит, так как требования он удовлетворяет, видимо. + Добавил центрирование после отЗУМления картинки.
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
-        let minZoomScale = scrollView.minimumZoomScale
-        let maxZoomScale = scrollView.maximumZoomScale
-        view.layoutIfNeeded()
-        let visibleRectSize = scrollView.bounds.size
-        let imageSize = image.size
-        let hScale = visibleRectSize.width / imageSize.width
-        let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
-        scrollView.setZoomScale(scale, animated: false)
-        scrollView.layoutIfNeeded()
-        let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width) / 2
-        let y = (newContentSize.height - visibleRectSize.height) / 2
-        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        let ratio = image.size.height / image.size.width
+        imageView.frame.size.width = scrollView.frame.width
+        imageView.frame.size.height = imageView.frame.size.width * ratio
+        scrollView.contentSize = scrollView.frame.size
+        let newY = scrollView.frame.height / 2 - imageView.frame.size.height / 2
+        scrollView.contentOffset = CGPoint(x: 0, y: -newY)
     }
     
     @IBAction private func pushBackButton(_ sender: Any) {
@@ -101,6 +62,16 @@ final class SingleImageViewController: UIViewController {
 extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        let horizontalInset = (scrollView.frame.size.width - imageView.frame.size.width) / 2
+        let verticalInset = (scrollView.frame.size.height - imageView.frame.size.height) / 2
+        if scale < 1 {
+            scrollView.contentInset = UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: verticalInset, left: 0, bottom: verticalInset, right: 0)
+        }
     }
 }
 
