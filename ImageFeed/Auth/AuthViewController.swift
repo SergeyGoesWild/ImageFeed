756647cf2fd11804eb806607ba66be09e5a53ff1
Sergey Telnov <sergej.telnov@gmail.com>
2024-09-17 00:30:02 +0200

    Добавил экран входа

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Sergey Telnov on 16/09/2024.
+//
+
+import Foundation
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
