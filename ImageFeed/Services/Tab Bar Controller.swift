184448a633a25ec5cd62229f6b58def42021e7cf
Sergey Telnov <sergej.telnov@gmail.com>
2024-10-05 00:22:32 +0200

    Сделан первый урок из верстки кодом

---
+++ ImageFeed/Services/Tab Bar Controller.swift
@@ -0,0 +1,25 @@
+//
+//  Tab Bar Controller.swift
+//  ImageFeed
+//
+//  Created by Sergey Telnov on 05/10/2024.
+//
+
+import Foundation
+import UIKit
+ 
+final class TabBarController: UITabBarController {
+       override func awakeFromNib() {
+           super.awakeFromNib()
+           let storyboard = UIStoryboard(name: "Main", bundle: .main)
+           let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+           let profileViewController = ProfileViewController()
+           profileViewController.tabBarItem = UITabBarItem(
+               title: "",
+               image: UIImage(named: "tab_profile_active"),
+               selectedImage: nil
+           )
+           self.viewControllers = [imagesListViewController, profileViewController]
+       }
+   }
+
